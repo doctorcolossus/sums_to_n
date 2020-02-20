2691ff5329145daaa343100abd41c5de09f24891
Casey Jones <cjone183@msudenver.edu>
2020-02-20 16:14:49 -0700

    import scanner; add documentation

--- SumsToN.java
+++ SumsToN.java
@@ -1,4 +1,16 @@
+/* CS 4050: Algorithms & Algorithm Analysis
+ * Project 1: Sums to N problem
+ * Casey Jones
+ * Bradley Isaacs
+ * Ahadwali Zadron
+ */
+
+Scanner console = new Scanner(System.in);
+
 public class SumsToN {
+
   public static void main (String [] args) {
+    Scanner console = new Scanner(System.in);
   }
+
 }
